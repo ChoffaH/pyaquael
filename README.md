429eaa2a20773fd630a0fd1e14aab45ca5854a7f
Choffa <choffa@haglund.uk>
2020-02-19 21:49:06 +0100

    New documentation and example

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # py-aquael
 A python library for the Aquael Leddy link unofficial API
+
+## Usage
+See [example.py](example.py) for examples on using library through import.
+
+This library can also be used from the command line:
+
+```
+Usage:
+  aquael poweron IPADDRESS RBW
+  aquael poweroff IPADDRESS
+  aquael (-h | --help)
+  aquael --version
+```
--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 import aquael
-import time
 
 hosts = [
   {
@@ -13,12 +12,11 @@ hosts = [
 ]
 hub = aquael.Hub(hosts)
 light = hub.lights[0]
-light.brightness = 100
-light.turn_on(200, 200, 112)
-light = hub.lights[1]
 light.brightness = 255
 light.turn_on(200, 200, 112)
+light.update()
+
 while True:
   if light.is_on:
-    print(f'Light is on? {light.is_on}')
+    print(f'Light is on? {light.is_on} {light.color}')
     break
