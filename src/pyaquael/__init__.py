031ce047b791b6228bcd68cf6a9b3d08792adad3
Choffa <choffa@haglund.uk>
2024-03-09 19:47:27 +0100

    Rewrite cli and release 0.4.0

--- src/pyaquael/__init__.py
+++ src/pyaquael/__init__.py
@@ -1,10 +1,32 @@
-from pyaquael import aquael
+"""Aquael CLI
+
+Usage:
+  aquael-cli (-h | --help)
+  aquael-cli --version
+  aquael-cli poweron IPADDRESS [--brightness BRIGHTNESS] [-r RED] [-b BLUE] [-w WHITE]
+  aquael-cli poweroff IPADDRESS
+
+Options:
+  -h --help                 Show this
+  --version                 Show version
+  --brightness BRIGHTNESS   Set brightness [default: 255]
+  -r RED                    Set red color [default: 0]
+  -b BLUE                   Set blue color [default: 0]
+  -w WHITE                  Set white color [default: 0]
+"""
+
+from importlib.metadata import version
 from docopt import docopt
+from pyaquael import aquael
 
 def main_cli():
-  arguments = docopt(__doc__, version='0.1.0')
+  arguments = docopt(__doc__, version=version('pyaquael'))
+  print(arguments)
   ip = arguments['IPADDRESS']
-  rbw = arguments['RBW']
+  brightness = int(arguments['--brightness'])
+  r = int(arguments['-r'])
+  b = int(arguments['-b'])
+  w = int(arguments['-w'])
   hosts = [
     {
       'name': 'Light 1',
@@ -16,6 +38,7 @@ def main_cli():
   light = hub.lights[0]
 
   if arguments['poweron'] is True:
-    light.turn_on(rbw)
+    light.brightness = brightness
+    light.turn_on(r, b, w)
   elif arguments['poweroff'] is True:
     light.turn_off()
