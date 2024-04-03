86d48c464a2ed10bc3a4653a531ea887132c325a
Christopher Haglund <christopher.haglund@sweco.se>
2024-04-03 21:26:19 +0200

    Add async functions and rebuild socket handling in sync functions

--- examples/simple.py
+++ examples/simple.py
@@ -1,4 +1,5 @@
 from pyaquael import aquael
+from time import sleep
 
 hosts = [
   {
@@ -11,12 +12,18 @@ hosts = [
   }
 ]
 hub = aquael.Hub(hosts)
-light = hub.lights[0]
-light.brightness = 150
-light.turn_on(200, 200, 112)
-light.update()
+light1 = hub.lights[0]
+light2 = hub.lights[1]
 
-while True:
-  if light.is_on:
-    print(f'Light is on? {light.is_on} {light.color}')
-    break
+light1.update()
+light2.turn_off()
+sleep(2)
+light2.brightness_pct = 1
+light2.turn_on(200, 200, 112)
+light2.update()
+
+if light1.is_on:
+  print(f'Light 1 is on? {light1.is_on} {light1.color}')
+
+if light2.is_on:
+  print(f'Light 2 is on? {light2.is_on} {light2.color}')
